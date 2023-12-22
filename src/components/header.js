ec852ed08f22ac57fa17d35d350db3c44111bb27
ice6 <ice6@live.cn>
2023-12-22 14:27:02 +0800

    chore: update deps

--- src/components/header.js
+++ src/components/header.js
@@ -1,7 +1,7 @@
 import { Link } from "gatsby"
 import PropTypes from "prop-types"
 import React from "react"
-import Img from "gatsby-image"
+import Img from "gatsby-plugin-image"
 import { StaticQuery, graphql } from "gatsby"
 
 const Header = ({ siteTitle, instrument }) => (
